46003d73e7b95e116a796b033048f5ab55a2176c
Kashif <kashiv.agnelite@gmail.com>
2016-03-19 18:41:55 -0300

    Added dummy data

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,4 +1,24 @@
 var meanApp = angular.module('meanApp', []);
 meanApp.controller('AppCtrl', ['$scope','$http', function ($scope, $http) {
-    console.log('Hello, World from controller');
+    // console.log('Hello, World from controller');
+    personOne = {
+        name: 'Tim',
+        email: 'tim@email.com',
+        number: '(111) 111-1111'
+    };
+
+    personTwo = {
+        name: 'Emily',
+        email: 'emily@email.com',
+        number: '(222) 222-2222'
+    };
+
+    personThree = {
+        name: 'John',
+        email: 'john@email.com',
+        number: '(333) 333-3333'
+    };
+
+    var people = [personOne, personTwo, personThree];
+    $scope.peopleList = people;
 }]);
